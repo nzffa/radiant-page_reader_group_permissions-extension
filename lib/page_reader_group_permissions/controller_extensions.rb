0b0c1424d11176f4886b57f7fdf765e0bb5e38f8
Craig Ambrose <craig@craigambrose.com>
2013-03-14 13:26:13 +1300

    remove debug code

--- lib/page_reader_group_permissions/controller_extensions.rb
+++ lib/page_reader_group_permissions/controller_extensions.rb
@@ -7,7 +7,6 @@ module PageReaderGroupPermissions
       :denied_message => "You must have group privileges to perform this action."
 
     def user_is_in_page_group
-      raise 'fish'
       return true if current_user.admin? || current_user.designer_or_developer?
 
       page = Page.find(params[:id] || params[:page_id] || params[:parent_id] || params[:page][:parent_id])
